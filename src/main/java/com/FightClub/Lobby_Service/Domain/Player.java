469b2ed14ecc66d8bfa53d45c9486cfd0532618f
SantiagoSu15 <sasupu08@gmail.com>
2026-03-06 08:02:37 -0500

    first commit

---
+++ src/main/java/com/FightClub/Lobby_Service/Domain/Player.java
@@ -0,0 +1,4 @@
+package com.FightClub.Lobby_Service.Domain;
+
+public class Player {
+}
---
+++ src/test/java/com/FightClub/Lobby_Service/LobbyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FightClub.Lobby_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
