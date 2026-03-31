f67f3526ba3b3b58035584582e6b276dcb4da997
SantiagoSu15 <sasupu08@gmail.com>
2026-03-31 12:15:37 -0500

    Event listener para websockets y desconexion

--- src/main/java/com/FightClub/Lobby_Service/LobbyServiceApplication.java
+++ src/main/java/com/FightClub/Lobby_Service/LobbyServiceApplication.java
@@ -2,8 +2,10 @@ package com.FightClub.Lobby_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class LobbyServiceApplication {
 
 	public static void main(String[] args) {
