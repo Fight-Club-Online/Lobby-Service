6ba232f64f0b3b2014b0eaa9bad3a87430b2c79a
SantiagoSu15 <sasupu08@gmail.com>
2026-04-08 13:54:53 -0500

    Dto para id como string

--- src/main/java/com/FightClub/Lobby_Service/Infrastructure/Inbound/Rest/DTO/Room/Socket/RoomStateMapperv2.java
+++ src/main/java/com/FightClub/Lobby_Service/Infrastructure/Inbound/Rest/DTO/Room/Socket/RoomStateMapperv2.java
@@ -2,11 +2,12 @@ package com.FightClub.Lobby_Service.Infrastructure.Inbound.Rest.DTO.Room.Socket;
 
 import com.FightClub.Lobby_Service.Domain.Model.Enums.RoomState;
 import com.FightClub.Lobby_Service.Domain.Model.Room;
-import com.FightClub.Lobby_Service.Infrastructure.Inbound.Rest.DTO.Room.RoomStateDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RoomStateMapperv2 {
+    @Mapping(target = "roomId", expression = "java(String.valueOf(room.getRoomId()))")
     RoomStateDTOv2 toDto(Room room);
 
     default String map(RoomState state) {
